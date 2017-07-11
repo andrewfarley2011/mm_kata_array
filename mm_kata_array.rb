d0ff682d0d419af6339695d19a7dc79eae866d79
andrewfarley2011 <andrewfarley@yahoo.com>
2017-07-11 12:04:04 -0400

    mm_kata_array_d

--- mm_kata_array.rb
+++ mm_kata_array.rb
@@ -6,19 +6,20 @@
 
 # |value,index| these can only be called within the method that the .each_with_index is being used with.
 # array[index] must be used within the function that the array was created and defined in.
+# |first,second| after the .each_with_index, first is always value, and second is always index position.
 
 def array()
 	array = [*1..100]
 		array.each_with_index do |value,index|
 		if value % 15 == 0
 			array[index] = "mined minds"
-			puts "mined minds"
+			puts array[index]
 		elsif value % 3 == 0
 			array[index] = "mined"
-			puts "mined"
+			puts array[index]
 		elsif value % 5 == 0
 			array[index] = "minds"
-			puts "minds"
+			puts array[index]
 		else
 			puts "#{value}"
 		end
