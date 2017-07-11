3e957ad4479c27c3d03737a08cd8e03631755407
andrewfarley2011 <andrewfarley@yahoo.com>
2017-07-10 23:18:55 -0400

    mm_kata_array

--- mm_kata_array.rb
+++ mm_kata_array.rb
@@ -5,20 +5,33 @@
 # end
 
 def array()
-	array = [*1..100].each_value
+	array = [*1..100]
+	array.each_with_index do |value,index|	
+		if value % 15 == 0
+			puts "mined minds"
+		elsif value % 3 == 0
+			puts "mined"
+		elsif value % 5 == 0
+			puts "minds"
+		else
+			puts "#{value}"
+		end
+	end	
 end
+
+array
 
 # array<< counter
 
-if array == 15
-	puts "mined minds"
-elsif array == 3
-	puts "mined"
-elsif array == 5
-	puts "minds"
-else
-	puts array
-end
+# if array == 15
+# 	puts "mined minds"
+# elsif array == 3
+# 	puts "mined"
+# elsif array == 5
+# 	puts "minds"
+# else
+# 	puts array
+# end
 
 # def number(x)
 # 	if x % 15 == 0
